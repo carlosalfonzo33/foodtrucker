d36d8bd52971643529ad276eecfc615781634a0d
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-06-14 14:53:26 -0700

    made change to zoom set to 15

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,13 +47,51 @@ $(document).on("click", "#food-search", callFood);
 
 //google maps 
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
-    mapTypeId: 'terrain'
-  });
-}
+//function initMap() {
+ // map = new google.maps.Map(document.getElementById('map'), {
+   // zoom: 2,
+   // center: new google.maps.LatLng(2.8,-187.3),
+    //mapTypeId: 'terrain'
+  //});
+//}
+ var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
